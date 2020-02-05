39e905c69356a39e996dd46f3b9c9e17a9c77006
sTranaeus <sebtranaeus@gmail.com>
2020-02-05 13:39:18 +0000

    Update TODOs. Improve get_single_user responses.

--- graph/app.py
+++ graph/app.py
@@ -5,6 +5,7 @@ import os
 
 app = Flask(__name__)
 # TODO: type anotation
+# TODO: use swagger.
 
 uri = os.getenv("NEO4J_URI")
 db_user = 'neo4j'
@@ -22,14 +23,15 @@ def hello_world():
     return 'Hello World!'
 
 
+# TODO: improve this function
 @app.route('/user/<email>', methods=['GET'])
 def get_single_user(email):
     with create_session() as session:
         response = session.read_transaction(get_user, email)
         user = response.single()
         if user:
-            return jsonify(dict(user['user'].items()))
-        return []
+            return jsonify(dict(user['user'].items())), 200
+        return "", 404
 
 
 if __name__ == '__main__':
