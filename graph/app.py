5705f536f31b1c2352ec86d8e67b10148780cf5d
k1889501 <giulio.di_zio@kcl.ac.uk>
2020-02-05 21:07:06 +0000

    Set api to use flask_restplus swagger

--- graph/app.py
+++ graph/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, g, jsonify
 from neo4j import GraphDatabase
-from .transaction_functions import get_user
+from transaction_functions import get_user, set_user_email
 import os
+from flask_restplus import Api, Resource, fields
 
 app = Flask(__name__)
+api = Api(app)
 # TODO: type anotation
 # TODO: use swagger.
+
+os.environ["NEO4J_URI"] = "bolt://35.246.56.244:7687"
+os.environ["NEO4J_PASSWORD"] = "L0nd0n&EU"
 
 uri = os.getenv("NEO4J_URI")
 db_user = 'neo4j'
@@ -18,19 +23,31 @@ def create_session():
     return g.neo4j_db
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
 
+user_email = api.model('User email', {'email' : fields.String('The new user email')}) #description for user emails.
 
-@app.route('/user/<email>', methods=['GET'])
-def get_single_user(email):
-    with create_session() as session:
-        response = session.read_transaction(get_user, email)
-        user = response.single()
-        if user:
-            return jsonify(dict(user['user'].items())), 200
-        return "", 404
+@api.route('/user/<email>')
+class user(Resource):
+    def get(self, email):
+        with create_session() as session:
+            response = session.read_transaction(get_user, email)
+            user = response.single()
+            if user:
+                return dict(user['user'].items()), 200
+            return "", 404
+
+    @api.expect(user_email)#You need to specify what is expected to be posted as body of the http message on this post.
+    def post(self, email):
+         with create_session() as session:
+            response = session.read_transaction(set_user_email, email, api.payload.get('email'))
+            user = response.single()
+            if user:
+                return dict(user['user'].items()), 200
+            return "", 404
+
 
 # TODO: support updating user info
 
--- graph/transaction_functions.py
+++ graph/transaction_functions.py
@@ -2,3 +2,5 @@
 def get_user(tx, user_email):
     return tx.run(f"MATCH (user {{email: '{user_email}'}}) return user")
 
+def set_user_email(tx, user_email, new_email):
+    return tx.run(f"MATCH (n:Person {{email: '{user_email}'}}) SET n.email = '{new_email}' RETURN n")
