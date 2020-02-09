10177a911e9646ce27b53e3c1829651dbc6d5c8a
danielidowu <55689201+danielidowu@users.noreply.github.com>
2020-02-09 16:53:10 +0000

    Add Empty State Screen

---
+++ Pintro/Screens/NetworkEmptyScreen.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useRef } from 'react';
+import { StyleSheet, Animated, Text, View, Button, Image } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import SignInUpButton from '../Components/SignInUpButton';
+import Colors from '../Constants/Colors';
+
+/**
+ * Empty Screen which allows the user to decide whether he wants to sign in
+ * or sign up. The screen consists of two custom buttons and the logo.
+ * @param {} props 
+ */
+const NetworkEmptyScreen = props => {
+    return (
+        <View style={styles.backGround}>
+            <View style={styles.main}>
+                <Animatable.View animation="fadeInDownBig">
+                <View style={styles.textContainer}>
+                    <Text style={styles.pintroText}>pintro</Text>
+                    <Text style={styles.yellowAccent}>.</Text>
+                </View>
+                </Animatable.View>
+                <View style={styles.imageContainer}>
+                    <Animatable.View animation="fadeInUpBig">
+                    <Image style={styles.imageContainer} source={require('../images/wifi2.png')}/>
+                    <View style={styles.textContainer}>
+                    <Text style={styles.messageText}>{'No Connection. \n Please Check Your Network.'}</Text>
+                </View>
+                    </Animatable.View>
+                    
+                </View>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    backGround: {
+        backgroundColor: Colors.pintroBlack,
+        flex: 1
+    },
+    main: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        //backgroundColor:'blue'
+    }, pintroText: {
+        color: 'white',
+        fontFamily: 'Poppins-Bold',
+        fontSize: 30
+    },
+    yellowAccent: {
+        color: Colors.pintroYellow,
+        fontSize: 30
+    },
+    buttonContainer: {
+        //backgroundColor:'green',
+        paddingTop: 20,
+        width: '70%',
+        fontFamily:'Poppins-Regular'
+    },
+    textContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 100
+    },
+    messageText : {
+        color: 'white',
+        fontFamily: 'Poppins-Bold',
+        textAlign: 'center',
+        paddingBottom: 100,
+        fontSize: 30
+    },
+    imageContainer: {
+        flex: 1,
+        width: null,
+        height: null,
+        paddingBottom: 25,
+        resizeMode: 'contain'
+    }
+});
+
+export default NetworkEmptyScreen 
