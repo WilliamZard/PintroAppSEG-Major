ded222fb39795438592ce1a6bc7f14cdaa83b742
Raffael Ryder <raffael.ryder@kcl.ac.uk>
2020-02-12 15:29:34 +0000

    Add standardised format for fonts

---
+++ Pintro/Constants/Fonts.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Stylesheet } from 'react-native';
+
+const styles = Stylesheet.create({
+    name_black: {
+        color: Colors.pintroBlack,
+        fontFamily: 'Poppins-Bold',
+        fontSize: 14
+    },
+    title_black: {
+        color: Colors.pintroBlack,
+        fontFamily: 'Poppins-Bold',
+        fontSize: 12
+    },
+    title_yellow: {
+        color: Colors.pintroYellow,
+        fontFamily: 'Poppins-Bold',
+        fontSize: 12
+    },
+    bio: {
+        color: grey,
+        fontFamily: 'Poppins-Light',
+        fontSize: 8
+    },
+    location: {
+        color: Colors.pintroBlack,
+        fontFamily: 'Poppins-Light',
+        fontSize: 8
+    },
+    tag_button_black: {
+        color: Colors.pintroBlack,
+        fontFamily: 'Poppins-Light',
+        fontSize: 10
+    },
+    tag_button_white: {
+        color: Colors.pintroWhite,
+        fontFamily: 'Poppins-Light',
+        fontSize: 10 
+    },
+});
