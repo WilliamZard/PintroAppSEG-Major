24c3f966e2110365ede5331e64b59b9f67ada163
sTranaeus <sebtranaeus@gmail.com>
2020-02-13 23:06:55 +0000

    Have get user by email requests return the user.

--- graph/graph_api/apis/neo4j_ops.py
+++ graph/graph_api/apis/neo4j_ops.py
@@ -23,7 +23,7 @@ def get_user_by_email(tx, user_email):
         tx = the context from where to run chipher statements and retreiving information from the db.
         user_email = the email of the user whose data needs to be retrieved.
     '''
-    return tx.run(f"MATCH (user:Person {{email: '{user_email}'}})")
+    return tx.run(f"MATCH (user:Person {{email: '{user_email}'}}) RETURN user")
 
 
 def delete_user_by_email(tx, user_email):
