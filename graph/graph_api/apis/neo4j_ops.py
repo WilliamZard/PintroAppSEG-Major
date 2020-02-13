962ebbfb9aac9ef67f564196a6d801140150037f
sTranaeus <sebtranaeus@gmail.com>
2020-02-13 22:45:58 +0000

    Refactor users API

---
+++ graph/graph_api/apis/neo4j_ops.py
@@ -0,0 +1,44 @@
+from neo4j import GraphDatabase
+from flask import g
+import os
+
+uri = os.getenv("NEO4J_URI")
+db_user = 'neo4j'
+password = os.getenv("NEO4J_PASSWORD")
+driver = GraphDatabase.driver(uri, auth=(db_user, password))
+
+
+def create_session():
+    if not hasattr(g, 'neo4j_db'):
+        g.neo4j_db = driver.session()
+    return g.neo4j_db
+
+
+def get_user_by_email(tx, user_email):
+    '''
+        Function that gets all the data related to a user with a particular email.
+        It returns a BoltStatementResult. 
+    '''
+    '''Args:
+        tx = the context from where to run chipher statements and retreiving information from the db.
+        user_email = the email of the user whose data needs to be retrieved.
+    '''
+    return tx.run(f"MATCH (user:Person {{email: '{user_email}'}})")
+
+
+def delete_user_by_email(tx, user_email):
+    return tx.run(f"MATCH (user:Person {{email: '{user_email}'}}) DELETE user")
+
+
+def set_user_fields(tx, user_email, fields):
+    '''
+        Function for setting a new email of a user which has a particular email saved in database.
+        It returns a BoltStatementResult containing the record of the edited user. 
+    '''
+    '''Args:
+        tx = the context from where to run chipher statements and retreiving information from the db.
+        user_email = the email of the user whose data needs to be edited.
+        new_email = the new email to assign to that user.
+    '''
+    return tx.run(f"MATCH (user:Person {{email: '{user_email}'}}) SET " +
+                  ", ".join("user.{}={}".format(k, v) for (k, v) in fields.items()))
---
+++ graph/graph_api/apis/users.py
@@ -0,0 +1,110 @@
+from flask_restplus import Namespace, Resource, fields
+from .neo4j_ops import create_session, get_user_by_email, delete_user_by_email, set_user_fields
+
+# TODO: add additional api details in parameters.
+# TODO: Update field titles
+api = Namespace('users', title='User related operations')
+users = api.model('Users', {
+    'email': fields.String(required=True, title='The user email.'),
+    # TODO: could do hashing in a function here
+    'password': fields.String(required=True, title='The user password.'),
+    'full_name': fields.String(required=True, title='The user full name.'),
+    'preferred_name': fields.String(title='The user preferred name.'),
+    'image': fields.String(title='image saved as array of Bytes representing the user\'s profile pic.'),
+    'phone': fields.String(title="The user's phone number."),
+    'gender': fields.String(title="The User's geneder"),
+    'job_title': fields.String(title='current job title of the user.'),
+    'location': fields.String(title='current city of the user.'),
+    'short_bio': fields.String(title='short bio describing the user of maximum 250 characters.'),
+    'story': fields.String(title='story describing the user of maximum 250 words.'),
+    'education': fields.String(title='Highest level obtained.')
+})  # title for accounts that needs to be created.
+
+
+@api.route('/user/<email>')
+@api.param('email', 'The email of the user')
+@api.response(404, 'User not found')
+class Users(Resource):
+    @api.doc('get_user')
+    @api.marshal_with(users)
+    def get(self, email):
+        # TODO: email validation, use a regex
+        '''Fetch a user given its email.'''
+        with create_session() as session:
+            response = session.read_transaction(get_user_by_email, email)
+            user = response.single()
+            if user:
+                return user.data, 200
+            return "User not found", 404
+
+    @api.doc('delete_user')
+    @api.response(204, 'User Deleted')
+    def delete(self, email):
+        '''Delete a user given its email.'''
+        # TODO: email validation
+        with create_session() as session:
+            response = session.read_transaction(delete_user_by_email, email)
+            # TODO: not sure how to handle neo4j response for this yet
+            user = response.single()
+            if user:
+                return user, 204
+            return "User not found", 404
+
+    @api.doc('create_user')
+    @api.expect(users)
+    @api.marshal_with(users, code=201)
+    def post(self):
+        # TODO: input validation
+        with create_session() as session:
+            response = session.read_transaction(create_user, api.payload())
+            # TODO: not sure how to handle neo4j response for this yet
+            user = response.single()
+            if user:
+                return user, 204
+            return "User not found", 404
+
+    # You need to specify what is expected to be posted as body of the http message on this post.
+    @api.expect(users)
+    @api.marshal_with(users)
+    def put(self, email):
+        # TODO: validate user email
+        # TODO: validate payload
+        with create_session() as session:
+            response = session.read_transaction(
+                set_user_fields, email, api.payload)
+            if response:
+                return 204
+            return "User was not found", 404
+
+
+"""
+@api.route('/signup')
+class SignUp(Resource):
+
+    @api.expect(account_model)
+    def post(self):
+        with create_session() as session:
+            response = session.read_transaction(create_user, api.payload)
+            if type(response) is dict:
+                return response
+
+            profile = response.single()
+            if profile:
+                return dict(profile['newUser'].items()), 200
+            return "User was not found", 404
+
+
+@api.route('/login')
+class LogIn(Resource):
+
+    @api.expect(login_credentials_model)
+    def post(self):
+        with create_session() as session:
+            profile = session.read_transaction(check_user, api.payload)
+            if type(profile) is dict:
+                return profile
+
+            if profile:
+                return dict(profile['p'].items()), 200
+            return "User was not found", 404
+"""
