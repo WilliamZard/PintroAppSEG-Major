c922fc248ba0e7c9385f31b9ef360225a38f6eb4
Raffael Ryder <raffyryder@gmail.com>
2020-02-19 22:27:07 +0000

    Create follow me, message me and edit components

---
+++ Pintro/Components/MsgMe.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View,Text,StyleSheet,TouchableOpacity } from 'react-native';
+import Colors from '../Constants/Colors';
+
+const MsgMe = props => {
+    return(
+
+        <TouchableOpacity style={styles.button} onPress={props.onPress} activeOpacity={0.6}>
+            <Text style={styles.buttonText}>{props.children}</Text>
+        </TouchableOpacity>
+
+    );
+};
+
+const styles = StyleSheet.create({
+    button:{
+        borderColor: Colors.pintroBlack,
+        borderWidth: 0.5,
+        paddingVertical:12,
+        width:'15%',
+        paddingHorizontal:30,
+        borderRadius:13,
+        marginTop:10,
+        marginRight:10
+    },
+    buttonText:{
+        color: Colors.pintroBlack,
+        textAlign: 'center',
+        fontFamily:'Poppins-Light'
+
+    }
+});
+
+export default MsgMe;
