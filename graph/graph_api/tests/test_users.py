cfb5c15cbc3bd905ff5742065d9be362d858ddd7
sTranaeus <sebtranaeus@gmail.com>
2020-02-23 16:07:18 +0000

    WIP Test Post endpoints.
    Tests erroring for now.

--- graph/graph_api/tests/test_users.py
+++ graph/graph_api/tests/test_users.py
@@ -12,6 +12,7 @@ from graph_api.apis.users import UserSchema
 
 from .generate_test_data import (NONEXISTANT_USER_EMAIL,
                                  VALID_USER, VALID_USER_TO_BE_DELETED, INVALID_EMAIL,
+                                 VALID_USER_TO_BE_UPDATED, VALID_USER_TO_BE_UPDATED_NEW_FIELDS,
                                  populate_db)
 
 
@@ -63,18 +64,43 @@ class TestGet:
 
 class TestDelete:
     # TODO: some duplicate code here for each endpoint test. Refactor.
-    def test_delete_user_with_valid_email_that_exists(app):
-        response = app.delete(f"/users/{VALID_USER_TO_BE_DELETED['email']}")
+    def test_delete_user_with_valid_email_that_exists(self, app):
+        email = VALID_USER_TO_BE_DELETED['email']
+        response = app.delete(f"/users/{email}")
         assert response.status == '204 OK'
         # TODO: consider using standard json.dumps instead of jsonify
         assert response.data == jsonify(VALID_USER_TO_BE_DELETED).data
 
-    def test_delete_user_with_valid_email_that_does_not_exist(app):
+        # Assert user was actually deleted in the database
+        response = app.get(f"/users/{email}")
+        assert response.status == '404 NOT FOUND'
+
+    def test_delete_user_with_valid_email_that_does_not_exist(self, app):
         response = app.delete(f"/users/{NONEXISTANT_USER_EMAIL}")
         assert response.status == '404 NOT FOUND'
         assert response.data == jsonify({}).data
 
-    def test_delete_user_with_invalid_email(app):
+    def test_delete_user_with_invalid_email(self, app):
         response = app.delete(f"/users/{INVALID_EMAIL}")
+        assert response.status == '422 Invalid Email'
+        assert response.data == jsonify({}).data
+
+
+class TestPut:
+    def test_put_user_with_valid_email_that_exists(self, app):
+        response = app.put(
+            f"/users/{VALID_USER_TO_BE_UPDATED['email']}", json=VALID_USER_TO_BE_UPDATED_NEW_FIELDS)
+        assert response.status == '204 No Content'
+        assert response.data == jsonify({}).data
+
+    def test_put_user_with_valid_email_that_does_not_exist(self, app):
+        response = app.put(
+            f"/users/{VALID_USER_TO_BE_UPDATED['email']}", json=VALID_USER_TO_BE_UPDATED_NEW_FIELDS)
+        assert response.status == '404 NOT FOUND'
+        assert response.data == jsonify({}).data
+
+    def test_put_user_with_invalid_email(self, app):
+        response = app.put(
+            f"/users/{VALID_USER_TO_BE_UPDATED['email']}", json=VALID_USER_TO_BE_UPDATED_NEW_FIELDS)
         assert response.status == '422 Invalid Email'
         assert response.data == jsonify({}).data
