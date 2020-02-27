243082d2a2d1e0c09e9a113b7c7596a932129985
sTranaeus <sebtranaeus@gmail.com>
2020-02-27 21:22:27 +0000

    Complete previous commit. Working but untested cloud function.

--- timeline/main.py
+++ timeline/main.py
@@ -4,18 +4,12 @@ from flask import make_response
 
 def generate_timeline(request):
     '''Fetch a users timeline by their email.'''
+    # TODO: test
+    # TODO: account for invalid email, non-existing email
     email = request.path[1:]  # exclude slash at beginning of path
     if not _valid_email(email):
         return make_response(b'', 422)
-
-    return requests.get('https://bluej-pintro-project.appspot.com/users/{email}')
-    """
-    if not _valid_email(email):
-        return 'Invalid email'
-    return 'timeline response'
-    base_url = ''
-    response = requests.get(base_url + f'/users/{email}')
-    """
+    return requests.get(f'https://bluej-pintro-project.appspot.com/users/{email}').content
 
 
 def _valid_email(email):
