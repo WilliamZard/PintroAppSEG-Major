3dffd69fb1f30e3603b3c4ade5437e124f1dd523
sTranaeus <sebtranaeus@gmail.com>
2020-02-27 20:25:19 +0000

    WIP call graph API in cloud function.

--- timeline/main.py
+++ timeline/main.py
@@ -4,15 +4,17 @@ from flask import make_response
 
 def generate_timeline(request):
     '''Fetch a users timeline by their email.'''
-    return request.path
+    email = request.path[1:]  # exclude slash at beginning of path
+    if not _valid_email(email):
+        return make_response(b'', 422)
 
+    return requests.get('https://bluej-pintro-project.appspot.com/users/{email}')
     """
     if not _valid_email(email):
         return 'Invalid email'
     return 'timeline response'
     base_url = ''
     response = requests.get(base_url + f'/users/{email}')
-    return make_response(response.content, 200)
     """
 
 
