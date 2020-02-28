9ae5f07893194127af6700b92827369b2cccbc22
k1889501 <giulio.di_zio@kcl.ac.uk>
2020-02-28 15:29:35 +0000

    Add FOLLOW relationships in test data

--- graph/graph_api/tests/generate_test_data.py
+++ graph/graph_api/tests/generate_test_data.py
@@ -5,7 +5,7 @@ import os
 from neo4j import GraphDatabase
 # TODO: format! line length is way too long
 SEB_NEO4J_IMPORT_DIR = '/Users/Seb/Library/Application Support/Neo4j Desktop/Application/neo4jDatabases/database-06fda328-94b7-4295-8d15-6dd8a4d56b97/installation-3.5.14/import/'
-GIULIO_IMPORT_DIR = r"C:\Users\giuli\Desktop"
+
 USERS_PROPERTIES = [
     "password",
     "profile_image",
@@ -19,7 +19,7 @@ USERS_PROPERTIES = [
     "preferred_name",
     "email",
     "story"]
-POSTED_PROPERTIES= ["date"]
+# POSTED_PROPERTIES= ["date"]
 POST_PROPERTIES = ["content"]
 VALID_USER = dict(zip(USERS_PROPERTIES, ['password', 'image', 'High School', 'Duke Wellington', 'male', '911',
                                          'not godless', 'strand', 'Duke', 'Duke', 'not_ucl@kcl.ac.uk', 'What is GKT?']))
@@ -40,10 +40,31 @@ USER_WITH_MULTIPLE_POSTS = dict(zip(USERS_PROPERTIES, ['password', 'image', 'UCL
 # The first 2 posts will be assigned to USER_WITH_MULTIPLE_POSTS
 USER_POST_A = dict(zip(POST_PROPERTIES, ['post2']))
 USER_POST_B = dict(zip(POST_PROPERTIES, ['post1']))
-USER_POST_C = dict(zip(POST_PROPERTIES, ['post3']))
+USER_POST_C = dict(zip(POST_PROPERTIES, ['post3']))# Third post is for posting tests
 
 POST_UPDATE_A = {'new_content': 'Hey I have just update my post content. This is POST_UPDATE_A'}
 POST_UPDATE_B = {'new_content': 'Hey I have just update my post content. This is POST_UPDATE_B'}
+
+USER_WITH_THREE_FOLLOWINGS = dict(zip(USERS_PROPERTIES, ['password', 'image', 'High School', 'Duke Wellington', 'male', '911',
+                                         'not godless', 'strand', 'Duke', 'Duke', 'yes_ucl@kcl.ac.uk', 'What is GKT?']))
+USER_WITH_TWO_FOLLOWINGS = dict(zip(USERS_PROPERTIES, ['password', 'image', 'Bachelor\'s', 'Leonardo Di Caprio', 'male', '000',
+                                         'I am the MVP', 'US', 'Actor', 'Lello', 'lello@gmail.com', 'I won an Oscar']))
+USER_WITH_ONE_FOLLOWING = dict(zip(USERS_PROPERTIES, ['password', 'image', 'Secondary school', 'John Jonny', 'male', '111',
+                                         'I am millionaire', 'London', 'Entrepreneur', 'Sweet guy', 'jj@gmail.com', 'I started as a taxi driver.']))
+USER_WITH_NO_FOLLOWINGS = dict(zip(USERS_PROPERTIES, ['password', 'image', 'Diploma', 'John Kennedy', 'male', '121',
+                                         'The only one pres', 'Unknown', 'Retired', 'JFK', 'jfk@gmail.com', 'They thought they killed me.']))  
+
+USER_WITH_THREE_FOLLOWINGS_POST_A = dict(zip(POST_PROPERTIES, ['USER_WITH_THREE_FOLLOWINGS first post']))
+USER_WITH_THREE_FOLLOWINGS_POST_B = dict(zip(POST_PROPERTIES, ['USER_WITH_THREE_FOLLOWINGS second post']))
+
+USER_WITH_TWO_FOLLOWINGS_POST_A = dict(zip(POST_PROPERTIES, ['USER_WITH_TWO_FOLLOWINGS first post']))                                      
+USER_WITH_TWO_FOLLOWINGS_POST_B = dict(zip(POST_PROPERTIES, ['USER_WITH_TWO_FOLLOWINGS second post']))
+USER_WITH_TWO_FOLLOWINGS_POST_C = dict(zip(POST_PROPERTIES, ['USER_WITH_TWO_FOLLOWINGS third post']))
+
+USER_WITH_ONE_FOLLOWING_POST_A = dict(zip(POST_PROPERTIES, ['USER_WITH_ONE_FOLLOWING first post']))
+
+USER_WITH_NO_FOLLOWING_POST_A = dict(zip(POST_PROPERTIES, ['USER_WITH_NO_FOLLOWINGS first post']))
+USER_WITH_NO_FOLLOWING_POST_B = dict(zip(POST_PROPERTIES, ['USER_WITH_NO_FOLLOWINGS second post']))
 
 NONEXISTANT_USER_EMAIL = 'does@exist.not'
 INVALID_EMAIL = 'invalidateme.now'
@@ -72,6 +93,8 @@ def populate_db(rewrite_test_data=False):
         session.write_transaction(create_user_email_existence_constraint)
         session.write_transaction(create_post_content_existence_constraint)
         session.write_transaction(create_test_data, test_data_file)
+        session.write_transaction(create_posts_to_people_in_test_db)
+        session.write_transaction(create_follows_relationships_to_people_in_test_db)
     # TODO: do this properly. Move all db stuff connect() function. Use yield.
     driver.close()
     print('Database populated')
@@ -90,7 +113,11 @@ def generate_test_data_csv():
         VALID_USER,
         VALID_USER_TO_BE_UPDATED,
         VALID_USER_TO_BE_DELETED,
-        USER_WITH_MULTIPLE_POSTS
+        USER_WITH_MULTIPLE_POSTS,
+        USER_WITH_THREE_FOLLOWINGS,
+        USER_WITH_TWO_FOLLOWINGS,
+        USER_WITH_ONE_FOLLOWING,
+        USER_WITH_NO_FOLLOWINGS  
     ]
     test_df = pd.DataFrame(data, columns=USERS_PROPERTIES)
     print(test_df)
@@ -120,12 +147,58 @@ def create_test_data(tx, test_data):
             email: csvLine.email,
             story: csvLine.story
             }})
-            FOREACH (_ IN CASE WHEN u.email = 'user_with_posts@gmail.com' THEN [1] ELSE [] END |
-                CREATE (a:Post {{content:'{USER_POST_A['content']}'}})
-                MERGE (u)-[:POSTED {{date: '{datetime.datetime.now()}'}}]->(a)
-                CREATE (b:Post {{content:'{USER_POST_B['content']}'}})
-                MERGE (u)-[:POSTED{{date: '{datetime.datetime.now()}'}}]->(b)
-            )"""
+            """
+    return tx.run(query)
+
+def create_posts_to_people_in_test_db(tx):
+    query = f"""MATCH (user_a:Person {{email:'{USER_WITH_THREE_FOLLOWINGS['email']}'}})
+                MATCH (user_b:Person {{email:'{USER_WITH_TWO_FOLLOWINGS['email']}'}})
+                MATCH (user_c:Person {{email:'{USER_WITH_ONE_FOLLOWING['email']}'}})
+                MATCH (user_d:Person {{email:'{USER_WITH_NO_FOLLOWINGS['email']}'}})
+                MATCH (user_e:Person {{email:'{USER_WITH_MULTIPLE_POSTS['email']}'}})
+                
+                CREATE (post_a:Post {{content:'{USER_WITH_THREE_FOLLOWINGS_POST_A['content']}'}})
+                CREATE (post_b:Post {{content:'{USER_WITH_THREE_FOLLOWINGS_POST_B['content']}'}})
+                CREATE (user_a)-[:POSTED {{date:'{datetime.datetime.now()}'}}]->(post_a)
+                CREATE (user_a)-[:POSTED {{date:'{datetime.datetime.now()}'}}]->(post_b)
+
+                CREATE (post_c:Post {{content:'{USER_WITH_TWO_FOLLOWINGS_POST_A['content']}'}})
+                CREATE (post_d:Post {{content:'{USER_WITH_TWO_FOLLOWINGS_POST_B['content']}'}})
+                CREATE (post_e:Post {{content:'{USER_WITH_TWO_FOLLOWINGS_POST_C['content']}'}})
+                CREATE (user_b)-[:POSTED {{date:'{datetime.datetime.now()}'}}]->(post_c)
+                CREATE (user_b)-[:POSTED {{date:'{datetime.datetime.now()}'}}]->(post_d)
+                CREATE (user_b)-[:POSTED {{date:'{datetime.datetime.now()}'}}]->(post_e)
+
+                CREATE (post_f:Post {{content:'{USER_WITH_ONE_FOLLOWING_POST_A['content']}'}})
+                CREATE (user_c)-[:POSTED {{date:'{datetime.datetime.now()}'}}]->(post_f)
+
+                CREATE (post_g:Post {{content:'{USER_WITH_NO_FOLLOWING_POST_A['content']}'}})
+                CREATE (post_h:Post {{content:'{USER_WITH_NO_FOLLOWING_POST_B['content']}'}})
+                CREATE (user_d)-[:POSTED {{date:'{datetime.datetime.now()}'}}]->(post_g)
+                CREATE (user_d)-[:POSTED {{date:'{datetime.datetime.now()}'}}]->(post_h)
+
+                CREATE (post_j:Post {{content:'{USER_POST_A['content']}'}})
+                CREATE (post_k:Post {{content:'{USER_POST_B['content']}'}})
+                CREATE (user_e)-[:POSTED {{date:'{datetime.datetime.now()}'}}]->(post_j)
+                CREATE (user_e)-[:POSTED {{date:'{datetime.datetime.now() + datetime.timedelta(0,1)}'}}]->(post_k)            
+             """
+    return tx.run(query)
+
+def create_follows_relationships_to_people_in_test_db(tx):
+    query = f"""MATCH (user_a:Person {{email:'{USER_WITH_THREE_FOLLOWINGS['email']}'}})
+                MATCH (user_b:Person {{email:'{USER_WITH_TWO_FOLLOWINGS['email']}'}})
+                MATCH (user_c:Person {{email:'{USER_WITH_ONE_FOLLOWING['email']}'}})
+                MATCH (user_d:Person {{email:'{USER_WITH_NO_FOLLOWINGS['email']}'}})
+
+                CREATE (user_a)-[:FOLLOWS]->(user_b)
+                CREATE (user_a)-[:FOLLOWS]->(user_c)
+                CREATE (user_a)-[:FOLLOWS]->(user_d)
+
+                CREATE (user_b)-[:FOLLOWS]->(user_c)
+                CREATE (user_b)-[:FOLLOWS]->(user_d)
+
+                CREATE (user_c)-[:FOLLOWS]->(user_d)
+             """
     return tx.run(query)
 
 
