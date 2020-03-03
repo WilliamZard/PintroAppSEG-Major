200d0eec3fae452211dc1bcedb7db1175bb13b1a
WilliamZard <william.zard@kcl.ac.uk>
2020-03-03 16:58:45 +0000

    create business profiles - Duplicate of users.py

---
+++ graph/graph_api/apis/businesses.py
@@ -0,0 +1,116 @@
+from flask.json import jsonify
+from flask import make_response
+from flask_restx import Namespace, Resource
+from flask_restx import fields as restx_fields
+from marshmallow import Schema, fields
+from marshmallow.exceptions import ValidationError
+from neo4j.exceptions import ConstraintError
+from .utils import valid_email
+
+from .neo4j_ops import (create_session, create_business, delete_business_by_email,
+                        get_business_by_email, set_business_fields)
+
+# TODO: enable swagger API spec
+# TODO: email validation
+
+
+api = Namespace('businesses', title='User related operations')
+
+# Schema used for serialisations
+
+
+class BusinessSchema(Schema):
+    email = fields.Email(required=True)
+    password = fields.Str(required=True)
+    full_name = fields.Str(required=True)
+    profile_image = fields.String()
+    phone = fields.String()
+    location = fields.String()
+    short_bio = fields.String()
+    story = fields.String()
+
+
+# Schema used for doc generation
+businesses = api.model('Users', {
+    'email': restx_fields.String(required=True, title='The user email.'),
+    'password': restx_fields.String(required=True, title='The user password.'),
+    'full_name': restx_fields.String(required=True, title='The user full name.'),
+    'profile_image': restx_fields.String(title='image saved as array of Bytes representing the user\'s profile pic.'),
+    'phone': restx_fields.String(title="The user's phone number."),
+    'location': restx_fields.String(title='current city of the user.'),
+    'short_bio': restx_fields.String(title='short bio describing the user of maximum 250 characters.'),
+    'story': restx_fields.String(title='story describing the user of maximum 250 words.')
+})  # title for accounts that needs to be created.
+
+business_schema = BusinessSchema()
+
+
+@api.route('/<string:email>')
+@api.produces('application/json')
+@api.expect(businesses)
+class Businesses(Resource):
+    def get(self, email):
+        '''Fetch a user given its email.'''
+        if not valid_email(email):
+            return make_response('', 422)
+
+        with create_session() as session:
+            response = session.read_transaction(get_business_by_email, email)
+            business = response.single()
+            if business:
+                # TODO: a lot going on here. See if this can be improved.
+                data = dict(business.data()['business'].items())
+                return jsonify(business_schema.dump(data))
+            return make_response('', 404)
+
+    @api.doc('delete_business')
+    @api.response(204, 'Business Deleted')
+    def delete(self, email):
+        '''Delete a business given its email.'''
+        if not valid_email(email):
+            return make_response('', 422)
+
+        with create_session() as session:
+            response = session.read_transaction(delete_business_by_email, email)
+            if response.summary().counters.nodes_deleted == 1:
+                return make_response('', 204)
+            return make_response('', 404)
+
+    @api.doc('update_business')
+    @api.response(204, 'Business Fields Deleted')
+    def put(self, email):
+        '''Update a business by the given fields.'''
+        if not valid_email(email):
+            return make_response('', 422)
+
+        # TODO: validate payload
+        with create_session() as session:
+            response = session.write_transaction(
+                set_business_fields, email, api.payload)
+            print(response.summary().counters)
+            if response.summary().counters.properties_set == len(api.payload):
+                return make_response('', 204)
+            return make_response('', 404)
+
+
+@api.route('/')
+@api.produces('application/json')
+@api.expect(businesses)
+class BusinessPost(Resource):
+    @api.doc('create_business')
+    @api.response(204, 'Business created')
+    @api.response(409, 'Business with that email already exists')
+    def post(self):
+        '''Create a business.'''
+        try:
+            deserialised_payload = business_schema.load(api.payload)
+        except ValidationError as e:
+            return make_response(e.messages['email'][0], 422)
+        with create_session() as session:
+            try:
+                response = session.write_transaction(
+                    create_business, deserialised_payload)
+                if response.summary().counters.nodes_created == 1:
+                    return make_response('', 201)
+            except ConstraintError:
+                return make_response('Node with that email already exists.', 409)
