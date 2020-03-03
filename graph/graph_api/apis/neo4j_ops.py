95ac19fa24c947069d121c2fc2bc626177ff66e7
WilliamZard <william.zard@kcl.ac.uk>
2020-03-03 17:12:07 +0000

    Add necessary functions toaccess business profiles in the database

--- graph/graph_api/apis/neo4j_ops.py
+++ graph/graph_api/apis/neo4j_ops.py
@@ -28,6 +28,7 @@ def close_db(error):
         g.neo4j_db.close()
 """
 
+""" Functions for Users """
 
 def get_user_by_email(tx, user_email):
     '''
@@ -63,5 +64,44 @@ def set_user_fields(tx, user_email, fields):
 
 def create_user(tx, fields):
     query = "CREATE (new_user: Person {" + ", ".join(
+        f"{k}: '{v}'" for (k, v) in fields.items()) + "})"
+    return tx.run(query)
+
+""" Functions for Businesses """
+
+def get_business_by_email(tx, business_email):
+    '''
+        Function that gets all the data related to a user with a particular email.
+        It returns a BoltStatementResult.
+    '''
+    '''Args:
+        tx = the context from where to run chipher statements and retreiving information from the db.
+        user_email = the email of the user whose data needs to be retrieved.
+    '''
+    return tx.run(f"MATCH (user:Business {{email: '{business_email}'}}) RETURN user")
+
+
+def delete_business_by_email(tx, business_email):
+    return tx.run(f"MATCH (user:Business {{email: '{business_email}'}}) DELETE user")
+
+
+def set_business_fields(tx, business_email, fields):
+    '''
+        Function for setting a new email of a user which has a particular email saved in database.
+        It returns a BoltStatementResult containing the record of the edited user.
+    '''
+    '''Args:
+        tx = the context from where to run chipher statements and retreiving information from the db.
+        user_email = the email of the user whose data needs to be edited.
+        new_email = the new email to assign to that user.
+    '''
+    # NOTE: this could error when assigning string values that need quotations
+    query = f"MATCH (user:Business {{email: '{business_email}'}}) SET " + \
+        ", ".join(f"user.{k}='{v}'" for (k, v) in fields.items())
+    return tx.run(query)
+
+
+def create_business(tx, fields):
+    query = "CREATE (new_user: Business {" + ", ".join(
         f"{k}: '{v}'" for (k, v) in fields.items()) + "})"
     return tx.run(query)
