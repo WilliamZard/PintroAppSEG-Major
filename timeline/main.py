f25784c9a6f1281ad285da20c8b05699ef239a2e
sTranaeus <sebtranaeus@gmail.com>
2020-03-04 22:10:00 +0000

    WIP Updating function use new graph_api endpoints

--- timeline/main.py
+++ timeline/main.py
@@ -2,16 +2,23 @@ import requests
 from flask import make_response
 
 
-def generate_timeline(request):
+def generate_timeline(email):  # request):
     '''Fetch a users timeline by their email.'''
     # TODO: test
     # TODO: account for invalid email, non-existing email
-    email = request.path[1:]  # exclude slash at beginning of path
-    if not _valid_email(email):
-        return make_response(b'', 422)
-    return requests.get(f'https://bluej-pintro-project.appspot.com/users/{email}').content
+    # email = request.path[1:]  # exclude slash at beginning of path
+    # if not _valid_email(email):
+    #    return make_response(b'', 422)
+    response = requests.get(
+        f'https://bluej-pintro-project.appspot.com/users/{email}/followings/posts').content
+    return response
 
 
 def _valid_email(email):
     import re
     return re.search(r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', email)
+
+
+if __name__ == '__main__':
+    response = generate_timeline('john.snow@winteriscoming.wes')
+    print(response)
