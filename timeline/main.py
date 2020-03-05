f110d556226f4cadf30d75954a6282707826ed54
sTranaeus <sebtranaeus@gmail.com>
2020-03-05 13:30:02 +0000

    Better mocking of request object for development. Not deployable for now.

--- timeline/main.py
+++ timeline/main.py
@@ -32,7 +32,13 @@ def generate_timeline(request):
 def _valid_email(email):
     return re.search(r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', email)
 
+
 # For dev
-# if __name__ == '__main__':
-#    response = generate_timeline('john.snow@winteriscoming.wes')
-#    print(response)
+if __name__ == '__main__':
+    from collections import namedtuple
+    MockRequest = namedtuple('Request', ['path'])
+    url = 'https://europe-west2-bluej-pintro-project.cloudfunctions.net/generate_timeline/\john.snow@winteriscoming.wes'.split(
+        '/')[-1]
+    call_cf_request = MockRequest(url)
+    response = generate_timeline(call_cf_request)
+    print(response)
