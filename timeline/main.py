b3ba35524861716ad00ee65872473660258519e0
sTranaeus <sebtranaeus@gmail.com>
2020-03-05 12:13:39 +0000

    Update CF to use new posts endpoint and to sort.
    WIP as errors on graph API side.

--- timeline/main.py
+++ timeline/main.py
@@ -1,24 +1,35 @@
 import requests
+import re
 from flask import make_response
+from marshmallow import Schema, fields
+from operator import itemgetter
 
 
-def generate_timeline(email):  # request):
+class PostSchema(Schema):
+    content = fields.Str(required=True)
+    user_email = fields.Email()
+    uuid = fields.UUID(required=True)
+    modified = fields.DateTime(required=True)
+
+
+def generate_timeline(request):
     '''Fetch a users timeline by their email.'''
-    # TODO: test
-    # TODO: account for invalid email, non-existing email
-    # email = request.path[1:]  # exclude slash at beginning of path
-    # if not _valid_email(email):
-    #    return make_response(b'', 422)
+    email = request.path[1:]  # exclude slash at beginning of path
+    if not _valid_email(email):
+        return make_response(b'', 422)
     response = requests.get(
-        f'https://bluej-pintro-project.appspot.com/users/{email}/followings/posts').content
-    return response
+        f'https://bluej-pintro-project.appspot.com/users/{email}/followings/posts')
+    posts_schema = PostSchema()
+    deserialised = posts_schema.load(response.json(), many=True)
+    deserialised.sort(key=itemgetter('modified'))
+
+    return deserialised
 
 
 def _valid_email(email):
-    import re
     return re.search(r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', email)
 
-
-if __name__ == '__main__':
-    response = generate_timeline('john.snow@winteriscoming.wes')
-    print(response)
+# For dev
+# if __name__ == '__main__':
+#    response = generate_timeline('john.snow@winteriscoming.wes')
+#    print(response)
