72cbf05ff3f2c9240fd7c60b028d30dadc3f739e
k1889501 <giulio.di_zio@kcl.ac.uk>
2020-03-07 18:01:25 +0000

    Create test data for business, spaces, and searches

--- graph/graph_api/tests/generate_test_data.py
+++ graph/graph_api/tests/generate_test_data.py
@@ -3,6 +3,8 @@ import datetime
 import os
 from neo4j import GraphDatabase
 from .test_data.users import *
+from .test_data.businesses import *
+from .test_data.spaces import *
 from .test_data.posts import *
 # TODO: organise test data. Different script?
 
@@ -47,6 +49,7 @@ def clear_db():
     with driver.session() as session:
         print("about to delete")
         session.write_transaction(_run_query, DELETE_ALL_NODES)
+        session.write_transaction(_run_query, DROP_SEARCH_INDEX)
 
 
 def _run_query(tx, query):
@@ -188,15 +191,89 @@ CONSTRAINT_POST_CONTENT_EXISTS = "CREATE CONSTRAINT ON(post: Post) ASSERT EXISTS
 
 DELETE_ALL_NODES = "MATCH(n) DETACH DELETE n"
 
+BUSINESSES_TO_TEST = [
+    VALID_BUSINESS,
+    VALID_BUSINESS_TO_BE_UPDATED,
+    VALID_BUSINESS_TO_BE_DELETED,
+    BUSINESS_WITH_MULTIPLE_POSTS,
+    BUSINESS_WITH_THREE_FOLLOWINGS,
+    BUSINESS_WITH_TWO_FOLLOWINGS,
+    BUSINESS_WITH_ONE_FOLLOWING,
+    BUSINESS_WITH_NO_FOLLOWINGS,
+    BUSINESS_ABOUT_TO_FOLLOW,
+    BUSINESS_ABOUT_TO_BE_FOLLOWED,
+    BUSINESS_FOLLOWING,
+    BUSINESS_BEING_FOLLOWED,
+    BUSINESS_WITH_FOLLOWINGS_THAT_HAVE_POSTS
+
+    
+]
+
+# TODO: restructure this
+CREATE_TEST_BUSINESS_DATA = ""
+for BUSINESS in BUSINESSES_TO_TEST:
+    CREATE_TEST_BUSINESS_DATA += "CREATE (" + BUSINESS['full_name'] + ":Business { "
+    CREATE_TEST_BUSINESS_DATA += "full_name: \'" + BUSINESS['full_name'] + "\' , "
+    CREATE_TEST_BUSINESS_DATA += "password: \'" + BUSINESS['password'] + "\' , "
+    CREATE_TEST_BUSINESS_DATA += "profile_image: \'" + BUSINESS['profile_image'] + "\' , "
+    CREATE_TEST_BUSINESS_DATA += "phone: \'" + BUSINESS['phone'] + "\' , "
+    CREATE_TEST_BUSINESS_DATA += "short_bio: \'" + BUSINESS['short_bio'] + "\' , "
+    CREATE_TEST_BUSINESS_DATA += "location: \'" + BUSINESS['location'] + "\' , "
+    CREATE_TEST_BUSINESS_DATA += "email: \'" + BUSINESS['email'] + "\' , "
+    CREATE_TEST_BUSINESS_DATA += "story: \'" + BUSINESS['story'] + "\'}) \n"
+
+
+CONSTRAINT_BUSINESS_EMAIL_UNIQUE = "CREATE CONSTRAINT ON(user: Business) ASSERT user.email IS UNIQUE"
+
+
+SPACES_TO_TEST = [
+    VALID_SPACE,
+    VALID_SPACE_TO_BE_UPDATED,
+    VALID_SPACE_TO_BE_DELETED,
+    SPACE_WITH_MULTIPLE_POSTS,
+    SPACE_WITH_THREE_FOLLOWINGS,
+    SPACE_WITH_TWO_FOLLOWINGS,
+    SPACE_WITH_ONE_FOLLOWING,
+    SPACE_WITH_NO_FOLLOWINGS,
+    SPACE_ABOUT_TO_FOLLOW,
+    SPACE_ABOUT_TO_BE_FOLLOWED,
+    SPACE_FOLLOWING,
+    SPACE_BEING_FOLLOWED,
+    SPACE_WITH_FOLLOWINGS_THAT_HAVE_POSTS
+]
+
+
+CREATE_TEST_SPACE_DATA = ""
+for SPACE in SPACES_TO_TEST:
+    CREATE_TEST_SPACE_DATA += "CREATE (" + SPACE['full_name'] + ":Space { "
+    CREATE_TEST_SPACE_DATA += "full_name: \'" + SPACE['full_name'] + "\' , "
+    CREATE_TEST_SPACE_DATA += "password: \'" + SPACE['password'] + "\' , "
+    CREATE_TEST_SPACE_DATA += "profile_image: \'" + SPACE['profile_image'] + "\' , "
+    CREATE_TEST_SPACE_DATA += "phone: \'" + SPACE['phone'] + "\' , "
+    CREATE_TEST_SPACE_DATA += "short_bio: \'" + SPACE['short_bio'] + "\' , "
+    CREATE_TEST_SPACE_DATA += "location: \'" + SPACE['location'] + "\' , "
+    CREATE_TEST_SPACE_DATA += "email: \'" + SPACE['email'] + "\' , "
+    CREATE_TEST_SPACE_DATA += "events: \'" + SPACE['events'] + "\'}) \n"
+
+
+CONSTRAINT_SPACE_EMAIL_UNIQUE = "CREATE CONSTRAINT ON(user: Space) ASSERT user.email IS UNIQUE"
+
+CREATE_SEARCH_INDEX = "CALL db.index.fulltext.createNodeIndex('SearchIndex', ['Person', 'Space', 'Business'], ['full_name', 'email', 'short_bio', 'events', 'story'])"
+DROP_SEARCH_INDEX = "CALL db.index.fulltext.drop(\"SearchIndex\")"
 queries = [
+    CREATE_SEARCH_INDEX,
     CONSTRAINT_POST_CONTENT_EXISTS,
     CONSTRAINT_USER_EMAIL_EXISTS,
     CONSTRAINT_USER_EMAIL_UNIQUE,
+    CONSTRAINT_SPACE_EMAIL_UNIQUE,
+    CREATE_TEST_SPACE_DATA,
+    CONSTRAINT_BUSINESS_EMAIL_UNIQUE,
+    CREATE_TEST_BUSINESS_DATA,
     CREATE_TEST_DATA,
     FOLLOWS_AA,
     CREATE_POSTS,
     CREATE_FOLLOWS_FOR_POSTS_USERS,
     RELATIONSHIPS_FOLLOWS_USER_A,
     RELATIONSHIPS_FOLLOWS_USER_B,
-    RELATIONSHIPS_FOLLOWS_USER_C
+    RELATIONSHIPS_FOLLOWS_USER_C,
 ]
