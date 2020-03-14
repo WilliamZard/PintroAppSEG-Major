ef0c26c32341f491e374569ff91b225693908d5d
Raffael Ryder <raffyryder@gmail.com>
2020-03-14 19:14:45 +0000

    Fix text styling

--- Pintro/Screens/SearchResults.js
+++ Pintro/Screens/SearchResults.js
@@ -10,8 +10,8 @@ const SearchResults = props => {
     const [searchKeyword,setSearchKeyword] = useState();
     
     return (
-        <View style={styles.pageContainer}>
-            <ScrollView style={styles.scrollContainer}>
+        <ScrollView style={styles.scrollContainer}>
+            <View style={styles.pageContainer}>
                 <View style={styles.topContainer}>
                     <Text style={styles.results}>Results (87)</Text>
                     <Text style={styles.found}>Found what you're looking for?</Text>
@@ -30,46 +30,58 @@ const SearchResults = props => {
                 </View>  
                 <View style={styles.peopleRow}>
                     <Text style={styles.sectionTitle}>People</Text>
-                    <Text style={styles.seeAll}>See all</Text>
+                    <Text style={styles.seeAll1}>See all</Text>
                 </View>
                 <View style={styles.imageRow}>
                     <TouchableOpacity style={styles.imageContainer}>
                         <Image source={require('../assets/blankImage.png')} style={styles.circleImage}/>
-                        <Text>Danielle Dodoo</Text>
+                        <Text style={fonts.title_black}>Danielle Dodoo</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.imageContainer}>
                         <Image source={require('../assets/blankImage.png')} style={styles.circleImage}/>
-                        <Text>Callum Thompson</Text>
+                        <Text style={fonts.title_black}>Callum Thompson</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.imageContainer}>
                         <Image source={require('../assets/blankImage.png')} style={styles.circleImage}/>
-                        <Text>Jane Doe</Text>
+                        <Text style={fonts.title_black}>Jane Doe</Text>
                     </TouchableOpacity>
                 </View>
                 <View>
                     <View style={styles.rowContainer}>
                         <Text style={styles.sectionTitle}>Companies</Text>
-                        <Text style={styles.seeAll}>See all</Text>
+                        <Text style={styles.seeAll2}>See all</Text>
                     </View>
-                    <Company style={styles.companies}/>
-                    <Company/>
+                    <Company 
+                        name={"Piin App Limited"} 
+                        bio={"Connect in Real Life with Piin App"}
+                    />
+                    <Company
+                        name={"Colour Coded Limited"}
+                        bio={"Promote your business with brnaded workwear"}
+                    />
                 </View>
                 <View>
                     <View style={styles.rowContainer}>
                         <Text style={styles.sectionTitle}>Groups</Text>
-                        <Text style={styles.seeAll}>See all</Text>
+                        <Text style={styles.seeAll1}>See all</Text>
                     </View>
-                    <Group style={styles.groups}/>
-                    <Group/>
+                    <Group
+                        name={"Group Name 1"}
+                        members={"100 Members"}    
+                    />
+                    <Group
+                        name={"Group Name 2"}
+                        members={"69 Members"}
+                    />
                 </View>       
-            </ScrollView>   
-        </View>
+              </View> 
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     pageContainer: {
-        backgroundColor: '#f1f1f2'
+        marginTop: 80,        
     },
     circleImage: {
         width: 110,
@@ -81,19 +93,28 @@ const styles = StyleSheet.create({
         color: Colors.pintroBlack,
         fontFamily: 'Poppins-Bold',
         fontSize: 12,
-        marginLeft: 20,
         marginBottom: 10,
+        textAlign: 'left',
+        marginLeft: 20,
     },
     rowContainer: {
         flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
-    seeAll: {
+    seeAll1: {
         color: 'grey',
         fontFamily: 'Poppins-Bold',
         fontSize: 10,
-        marginLeft: 250,
+        textAlign: 'right',
+        marginTop: 2.5,
+        marginLeft: 295
+    },
+    seeAll2: {
+        color: 'grey',
+        fontFamily: 'Poppins-Bold',
+        fontSize: 10,
+        textAlign: 'right',
+        marginTop: 2.5,
+        marginLeft: 268
     },
     imageRow: {
         flexDirection: 'row',
@@ -107,7 +128,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     scrollContainer: {
-        marginTop: 80,
+        backgroundColor: '#f1f1f2'
     },
     searchText: {
         color: 'grey',
@@ -133,8 +154,6 @@ const styles = StyleSheet.create({
     },
     peopleRow: {
         flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
         marginTop: 20,
     },
 });
