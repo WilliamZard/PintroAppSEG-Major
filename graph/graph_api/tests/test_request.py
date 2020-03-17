d868497f4a512b4bafaab907563f61421c3bf897
sTranaeus <sebtranaeus@gmail.com>
2020-03-17 18:47:32 +0000

    POST request all tests passing.

--- graph/graph_api/tests/test_request.py
+++ graph/graph_api/tests/test_request.py
@@ -5,6 +5,8 @@ from .generate_test_data import FOLLOW_REQUESTER_A, FOLLOW_REQUESTER_B
 
 @pytest.mark.POST_request
 class TestPOST:
+    # TODO: add tests for entering a valid email
+    # TODO: add tests for if given users exist or not
     def test_POST_follow_request_with_valid_users(self, app):
         response = app.post(
             f"/request/follow/{FOLLOW_REQUESTER_A['email']}/{FOLLOW_REQUESTER_B['email']}")
@@ -14,7 +16,12 @@ class TestPOST:
         # TODO: add get request for checking if FOLLOW_REQUEST relationship was actually created
 
     def test_POST_affiliation_request_with_valid_users(self, app):
-        pass
+        response = app.post(
+            f"/request/affiliation/{FOLLOW_REQUESTER_A['email']}/{FOLLOW_REQUESTER_B['email']}")
+        assert response.status == '201 CREATED'
+        assert response.data == b''
+
+        # TODO: add get request for checking if FOLLOW_REQUEST relationship was actually created
 
 
 @pytest.mark.DELETE_request
