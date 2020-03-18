416ebce4ca0a96177c5b482abd5c3f6fcd5feccf
sTranaeus <sebtranaeus@gmail.com>
2020-03-18 18:28:31 +0000

    Make it easier for client to use GET notifications response.
    Now the response contains relationship types that can be used in the request URL, avoiding the client needing to store a mapping.

--- graph/graph_api/apis/notifications.py
+++ graph/graph_api/apis/notifications.py
@@ -3,7 +3,10 @@ from flask_restx import fields as restx_fields
 from marshmallow import Schema, fields
 from flask import make_response
 from .neo4j_ops import create_session, get_notifications
+from .request import REQUEST_RELATIONSHIPS
 
+REVERSED_REQUEST_RELATIONSHIPS = {
+    value: key for key, value in REQUEST_RELATIONSHIPS.items()}
 
 api = Namespace('notifications',
                 title='Endpoint for operation on notifications.')
@@ -33,5 +36,7 @@ class Notifications(Resource):
         with create_session() as session:
             response = session.read_transaction(get_notifications, user_email)
             if response:
-                return notifications_schema.dump(response.data(), many=True)
+                data = [{key: REVERSED_REQUEST_RELATIONSHIPS[notification[key]] if notification[key] in REVERSED_REQUEST_RELATIONSHIPS else notification[key] for key in notification}
+                        for notification in response.data()]
+                return notifications_schema.dump(data, many=True)
             return make_response('', 400)
