492957eeeed7c51394a23663e422a1c502114bff
RaffWasTaken <47249441+RaffWasTaken@users.noreply.github.com>
2020-03-23 13:39:26 +0000

    Import message png and add it to help me with button

--- Pintro/Components/HelpMeWith.js
+++ Pintro/Components/HelpMeWith.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View,Text,StyleSheet,TouchableOpacity} from 'react-native';
+import { Text, StyleSheet, TouchableOpacity, Image, View } from 'react-native';
 import Colors from '../Constants/Colors';
 import { fonts } from '../Constants/Fonts';
 
@@ -7,8 +7,11 @@ const HelpMeWith = props => {
     return(
 
         <TouchableOpacity style={styles.tag} onPress={props.onPress} activeOpacity={0.6}>
-                <Text style={fonts.title_black}>Help us with</Text>
-                <Text style={fonts.bio}>{props.children}</Text>
+                <View>
+                    <Text style={fonts.title_black}>Help us with</Text>
+                    <Text style={fonts.bio}>{props.children}</Text>
+                </View>
+                <Image source={require('../assets/helpMeMsg.png')} style={{height: 50, width: 50, marginLeft: 70}}/> 
         </TouchableOpacity> 
 
     );
@@ -16,16 +19,18 @@ const HelpMeWith = props => {
 
 const styles = StyleSheet.create({
     tag: {
-        borderColor: Colors.pintroBlack,
+        borderColor: 'grey',
         color: Colors.pintroWhite,
         borderWidth:0.5,
-        width:'15%',
         paddingVertical:12,
         paddingHorizontal:30,
         borderRadius:20,
         marginRight:10,
         marginTop:15,
-        marginBottom:10
+        marginBottom:10,
+        height: 80,
+        width: 250,
+        flexDirection: 'row'
     },
     tag_text: {
         color: Colors.pintroBlack,
