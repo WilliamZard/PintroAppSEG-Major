b1ff8a5492bdb50f02ec53c01ab53ed46050fea0
sTranaeus <sebtranaeus@gmail.com>
2020-03-24 11:19:42 +0000

    Adapt POST tags to new testing approach.

--- graph/graph_api/tests/test_data/tags.py
+++ graph/graph_api/tests/test_data/tags.py
@@ -5,7 +5,7 @@ from collections import namedtuple
 # TODO: this is duplicated across testing scripts. Refactor.
 NOW = str(datetime.datetime.now()).replace(' ', 'T') + '000' + '+00:00'
 TAG_PROPERTIES = ['uuid', 'created', 'name']
-TAG_DEFAULTS = [uuid.uuid4(), NOW, 'Default Name']
+TAG_DEFAULTS = [str(uuid.uuid4()), NOW, 'Default Name']
 
 Tag = namedtuple('Tag', TAG_PROPERTIES, defaults=TAG_DEFAULTS)
 
