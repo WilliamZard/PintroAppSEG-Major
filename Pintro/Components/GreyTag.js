b598f79a02535ba74b1948c3f69d23c61edebe8b
RaffWasTaken <47249441+RaffWasTaken@users.noreply.github.com>
2020-03-26 16:00:37 +0000

    Create a tag that changes color on press

---
+++ Pintro/Components/GreyTag.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { View,Text,TouchableOpacity } from 'react-native';
+import Colors from '../Constants/Colors.js';
+
+const GreyTag = props => {
+    const greyTag = {
+            borderColor: '#c2c2c4',
+            backgroundColor: '#c2c2c4',
+            borderWidth: 0.5,
+            paddingVertical: 10,
+            paddingHorizontal: 28,
+            borderRadius: 24,
+            marginHorizontal: 5,
+        };
+
+        const blackTag = {
+            borderColor: Colors.pintroBlack,
+            backgroundColor: Colors.pintroBlack,
+            borderWidth: 0.5,
+            paddingVertical: 10,
+            paddingHorizontal: 28,
+            borderRadius: 24,
+            marginHorizontal: 5,
+        };
+
+        const blackText = {
+            color: Colors.pintroBlack,
+            textAlign: 'center',
+            fontFamily: 'Poppins-Regular',
+            fontSize: 12
+        }
+
+        const whiteText = {
+            color: Colors.pintroWhite,
+            textAlign: 'center',
+            fontFamily: 'Poppins-Regular',
+            fontSize: 12
+        }
+
+    const[tagStyle,setTag] = useState(greyTag);
+    const[tagText,setText] = useState(blackText);
+    const[selected,select] = useState(false);
+
+    
+
+    function onTagPress() {
+        if(selected) {
+            setTag(greyTag);
+            setText(blackText);
+            select(false);
+        } else {
+            setTag(blackTag);
+            setText(whiteText);
+            select(true);
+        }
+    }
+
+    return(
+        <TouchableOpacity onPress={() => onTagPress()} activeOpacity={0.6}>
+            <View style={tagStyle}>
+                <Text style={tagText}>{props.children}</Text>
+            </View>
+        </TouchableOpacity> 
+    );
+};
+
+
+export default GreyTag;
