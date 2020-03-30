22e07d04e34e80420573d93c7ceba499efbd127d
sTranaeus <sebtranaeus@gmail.com>
2020-03-30 19:22:07 +0100

    Clean up test_search

--- graph/graph_api/tests/test_search.py
+++ graph/graph_api/tests/test_search.py
@@ -3,7 +3,9 @@ from flask import json
 from flask.json import jsonify
 
 from .conftest import app, populate_db
-from .generate_test_data import User, Space, Business, Tag, basic_space_node, basic_business_node, basic_user_node, basic_tag_node
+from .generate_test_data import (Business, Space, Tag, User,
+                                 basic_business_node, basic_space_node,
+                                 basic_tag_node, basic_user_node)
 from .helper_functions import *
 
 
@@ -13,21 +15,21 @@ class TestPOST:
         ''' A query done with a string of length 1 shouldn't return any node because otherwise it would basically
             return all the nodes in the database
         '''
-        #Generate Test data 
+        # Generate Test data
         nodes = []
-        space_data =[]
+        space_data = []
         space_data.append(Space(email='space_a@test.com')._asdict())
         space_data.append(Space(email='space_b@test.com')._asdict())
-        for space in space_data:    
+        for space in space_data:
             nodes.append(basic_space_node(space))
-        
-        business_data=[]
+
+        business_data = []
         business_data.append(Business(email='business_a@test.com')._asdict())
         business_data.append(Business(email='business_b@test.com')._asdict())
         for business in business_data:
             nodes.append(basic_business_node(business))
 
-        user_data=[]
+        user_data = []
         user_data.append(User(email='user_a@test.com')._asdict())
         user_data.append(User(email='user_b@test.com')._asdict())
         for user in user_data:
@@ -36,32 +38,33 @@ class TestPOST:
             nodes.append(basic_user_node(user))
         populate_db(nodes_to_create=nodes)
 
-        #Test
+        # Test
         string_query = 'a'
         response = app.post("/search/", json=dict({'query': string_query}))
-        #Assertion
+        # Assertion
         assert response.status == '200 OK'
         assert response.get_json() == []
 
     def test_empty_query_should_return_nothing(self, app, populate_db):
-        ''' A query done with a string of length 0 shouldn't return any node because otherwise it would 
-            return all the nodes in the database which doesn't make sense in any situation
         '''
-        #Generate Test data 
+        A query done with a string of length 0 shouldn't return any node because otherwise it would 
+        return all the nodes in the database which doesn't make sense in any situation.
+        '''
+        # Generate Test data
         nodes = []
-        space_data =[]
+        space_data = []
         space_data.append(Space(email='space_a@test.com')._asdict())
         space_data.append(Space(email='space_b@test.com')._asdict())
-        for space in space_data:    
+        for space in space_data:
             nodes.append(basic_space_node(space))
-        
-        business_data=[]
+
+        business_data = []
         business_data.append(Business(email='business_a@test.com')._asdict())
         business_data.append(Business(email='business_b@test.com')._asdict())
         for business in business_data:
             nodes.append(basic_business_node(business))
 
-        user_data=[]
+        user_data = []
         user_data.append(User(email='user_a@test.com')._asdict())
         user_data.append(User(email='user_b@test.com')._asdict())
         for user in user_data:
@@ -70,35 +73,36 @@ class TestPOST:
             nodes.append(basic_user_node(user))
         populate_db(nodes_to_create=nodes)
 
-        #Test
+        # Test
         string_query = ''
         response = app.post("/search/", json=dict({'query': string_query}))
-        #Assertion
+        # Assertion
         assert response.status == '200 OK'
         assert response.get_json() == []
 
-    def test_query_that_partially_matches_full_names_or_emails_should_return_correct_nodes(self,app, populate_db):
-        ''' A query that partially matches some full names or emails should return all the appropriate accounts.
-        '''
-        #Generate Test data 
+    def test_query_that_partially_matches_full_names_or_emails_should_return_correct_nodes(self, app, populate_db):
+        ''' A query that partially matches some full names or emails should return all the appropriate accounts.'''
+        # Generate Test data
         nodes = []
-        space_data =[]
-        matching_space = Space(email='matching_space@test.com', full_name='Lello Pasqualino')._asdict()
+        space_data = []
+        matching_space = Space(
+            email='matching_space@test.com', full_name='Lello Pasqualino')._asdict()
         space_data.append(matching_space)
         space_data.append(Space(email='space_b@test.com')._asdict())
         space_data.append(Space(email='space_c@test.com')._asdict())
-        for space in space_data:    
+        for space in space_data:
             nodes.append(basic_space_node(space))
-        
-        business_data=[]
-        matching_business = Business(email='matching_business@test.com', full_name='Lello Di Caprio')._asdict()
+
+        business_data = []
+        matching_business = Business(
+            email='matching_business@test.com', full_name='Lello Di Caprio')._asdict()
         business_data.append(matching_business)
         business_data.append(Business(email='business_b@test.com')._asdict())
         business_data.append(Business(email='business_c@test.com')._asdict())
         for business in business_data:
             nodes.append(basic_business_node(business))
 
-        user_data=[]
+        user_data = []
         matching_user = User(email='Lello@test.com')._asdict()
         user_data.append(matching_user)
         user_data.append(User(email='user_b@test.com')._asdict())
@@ -109,33 +113,37 @@ class TestPOST:
             nodes.append(basic_user_node(user))
         populate_db(nodes_to_create=nodes)
 
-        #Test
+        # Test
         string_query = 'Lello'
         response = app.post("/search/", json=dict({'query': string_query}))
-        json_response = prepere_search_responses_for_account_assertion(response)
+        json_response = prepere_search_responses_for_account_assertion(
+            response)
 
         json_response = ordered(json_response)
-        expected_accounts = ordered([matching_space, matching_business, matching_user])
-        #Assertion
+        expected_accounts = ordered(
+            [matching_space, matching_business, matching_user])
+        # Assertion
         assert response.status == '200 OK'
         assert json_response == expected_accounts
 
-    def test_query_that_partially_matches_stories_should_return_correct_nodes(self,app, populate_db):
-        ''' A query that partially matches some stories in any business account, normal user account, 
-            or space should return all the appropriated accounts.
+    def test_query_that_partially_matches_stories_should_return_correct_nodes(self, app, populate_db):
         '''
-        #Generate Test data 
+        A query that partially matches some stories in any business account, normal user account, 
+        or space should return all the appropriated accounts.
+        '''
+        # Generate Test data
         nodes = []
-        
-        business_data=[]
-        matching_business = Business(email='matching_business@test.com', story='It is a business sponsored by UCL.')._asdict()
+
+        business_data = []
+        matching_business = Business(
+            email='matching_business@test.com', story='It is a business sponsored by UCL.')._asdict()
         business_data.append(matching_business)
         business_data.append(Business(email='business_b@test.com')._asdict())
         business_data.append(Business(email='business_c@test.com')._asdict())
         for business in business_data:
             nodes.append(basic_business_node(business))
 
-        user_data=[]
+        user_data = []
         matching_user = User(email='ucl@test.com')._asdict()
         user_data.append(matching_user)
         user_data.append(User(email='user_b@test.com')._asdict())
@@ -146,41 +154,46 @@ class TestPOST:
             nodes.append(basic_user_node(user))
         populate_db(nodes_to_create=nodes)
 
-        #Test
+        # Test
         string_query = 'UCL'
         response = app.post("/search/", json=dict({'query': string_query}))
-        json_response = prepere_search_responses_for_account_assertion(response)
+        json_response = prepere_search_responses_for_account_assertion(
+            response)
         json_response = ordered(json_response)
         expected_accounts = ordered([matching_business, matching_user])
-        #Assertion
+        # Assertion
         assert response.status == '200 OK'
         assert json_response == expected_accounts
-    
-    def test_query_that_partially_matches_short_bios_should_return_correct_nodes(self,app, populate_db):
-        ''' A query that partially matches some short bios in any business account, normal user account, 
-            or space should return all the appropriated accounts.
+
+    def test_query_that_partially_matches_short_bios_should_return_correct_nodes(self, app, populate_db):
         '''
-        #Generate Test data 
+        A query that partially matches some short bios in any business account, normal user account, 
+        or space should return all the appropriated accounts.
+        '''
+        # Generate Test data
         nodes = []
 
-        space_data =[]
-        matching_space = Space(email='matching_space@test.com', short_bio='This team makes researches on COVID-19')._asdict()
+        space_data = []
+        matching_space = Space(email='matching_space@test.com',
+                               short_bio='This team makes researches on COVID-19')._asdict()
         space_data.append(matching_space)
         space_data.append(Space(email='space_b@test.com')._asdict())
         space_data.append(Space(email='space_c@test.com')._asdict())
-        for space in space_data:    
+        for space in space_data:
             nodes.append(basic_space_node(space))
-        
-        business_data=[]
-        matching_business = Business(email='matching_business@test.com', short_bio='We are a company fighting against COVID-19')._asdict()
+
+        business_data = []
+        matching_business = Business(email='matching_business@test.com',
+                                     short_bio='We are a company fighting against COVID-19')._asdict()
         business_data.append(matching_business)
         business_data.append(Business(email='business_b@test.com')._asdict())
         business_data.append(Business(email='business_c@test.com')._asdict())
         for business in business_data:
             nodes.append(basic_business_node(business))
 
-        user_data=[]
-        matching_user = User(email='Lello@test.com', short_bio='I got COVID-19 once.')._asdict()
+        user_data = []
+        matching_user = User(email='Lello@test.com',
+                             short_bio='I got COVID-19 once.')._asdict()
         user_data.append(matching_user)
         user_data.append(User(email='user_b@test.com')._asdict())
         user_data.append(User(email='user_c@test.com')._asdict())
@@ -189,23 +202,26 @@ class TestPOST:
             user.pop('help_others')
             nodes.append(basic_user_node(user))
         populate_db(nodes_to_create=nodes)
-        
-        #Test
+
+        # Test
         string_query = 'COVID'
         response = app.post("/search/", json=dict({'query': string_query}))
 
-        json_response = prepere_search_responses_for_account_assertion(response)
+        json_response = prepere_search_responses_for_account_assertion(
+            response)
         json_response = ordered(json_response)
-        expected_accounts = ordered([matching_space, matching_business, matching_user])
-        #Assertion
+        expected_accounts = ordered(
+            [matching_space, matching_business, matching_user])
+        # Assertion
         assert response.status == '200 OK'
         assert json_response == expected_accounts
 
     def test_query_that_partially_matches_tag_returns_correct_user(self, app, populate_db):
-        ''' A query that partially matches a tag used in any business account, normal user account, 
-            or space should return all the appropriated accounts.
         '''
-        #Generate Test data 
+        A query that partially matches a tag used in any business account, normal user account, 
+        or space should return all the appropriated accounts.
+        '''
+        # Generate Test data
         nodes = []
         tag_data = []
         matching_tag = Tag(name='Civil Engineering')._asdict()
@@ -215,23 +231,25 @@ class TestPOST:
         for tag in tag_data:
             nodes.append(basic_tag_node(tag))
 
-        business_data=[]
-        matching_business_a = Business(email='matching_business_a@test.com')._asdict()
-        matching_business_b = Business(email='matching_business_b@test.com')._asdict()
+        business_data = []
+        matching_business_a = Business(
+            email='matching_business_a@test.com')._asdict()
+        matching_business_b = Business(
+            email='matching_business_b@test.com')._asdict()
         business_data.append(matching_business_a)
         business_data.append(matching_business_b)
         business_data.append(Business(email='business_b@test.com')._asdict())
         business_data.append(Business(email='business_c@test.com')._asdict())
         for business in business_data:
             nodes.append(basic_business_node(business))
-        
-        space_data =[]
+
+        space_data = []
         space_data.append(Space(email='space_b@test.com')._asdict())
         space_data.append(Space(email='space_c@test.com')._asdict())
-        for space in space_data:    
+        for space in space_data:
             nodes.append(basic_space_node(space))
 
-        user_data=[]
+        user_data = []
         user_data.append(User(email='user_b@test.com')._asdict())
         user_data.append(User(email='user_c@test.com')._asdict())
         for user in user_data:
@@ -249,15 +267,17 @@ class TestPOST:
             'e_node_properties': {'name': matching_tag['name']}, 'e_node_labels': 'Tag',
             'relationship_type': 'TAGGED'}
 
-        populate_db(nodes_to_create=nodes, relationships_to_create=[tag_a, tag_b])
-        
-        #Test
+        populate_db(nodes_to_create=nodes,
+                    relationships_to_create=[tag_a, tag_b])
+
+        # Test
         string_query = 'Civil'
         response = app.post("/search/", json=dict({'query': string_query}))
 
-        json_response = prepere_search_responses_for_account_assertion(response)
+        json_response = prepere_search_responses_for_account_assertion(
+            response)
         json_response = ordered(json_response)
         expected_accounts = ordered([matching_business_a, matching_business_b])
-        #Assertion
+        # Assertion
         assert response.status == '200 OK'
         assert json_response == expected_accounts
