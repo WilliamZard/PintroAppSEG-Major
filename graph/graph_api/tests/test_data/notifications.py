55456416271d4d36aa57d7e3454699769b18c69c
sTranaeus <sebtranaeus@gmail.com>
2020-03-30 20:08:59 +0100

    Remove redundant code.

--- graph/graph_api/tests/test_data/notifications.py
+++ graph/graph_api/tests/test_data/notifications.py
@@ -1,5 +1,4 @@
 from collections import namedtuple
-# TODO: add created property to relationships
 NOTIFICATION_PROPERTIES = [
     'requester_email',
     'recipient_email',
@@ -15,11 +14,3 @@ NOTIFICATION_DEFAULTS = [
 
 Notification = namedtuple(
     'Notification', NOTIFICATION_PROPERTIES, defaults=NOTIFICATION_DEFAULTS)
-
-"""
-NOTIFICATION_A = dict(zip(NOTIFICATION_PROPERTIES, [
-                      USER_REQUESTING_USER_WITH_NOTIFICATIONS_A['email'], USER_WITH_NOTIFICATIONS['email'], 'follow']))
-
-NOTIFICATION_B = dict(zip(NOTIFICATION_PROPERTIES, [
-                      BUSINESS_REQUESTING_AFFILIATION_TO_USER['email'], USER_WITH_NOTIFICATIONS['email'], 'affiliation']))
-"""
--- graph/graph_api/tests/test_data/tags.py
+++ graph/graph_api/tests/test_data/tags.py
@@ -8,15 +8,3 @@ TAG_PROPERTIES = ['uuid', 'created', 'name']
 TAG_DEFAULTS = [str(uuid.uuid4()), NOW, 'Default Name']
 
 Tag = namedtuple('Tag', TAG_PROPERTIES, defaults=TAG_DEFAULTS)
-"""
-# Using custom labels for testing to keep things simple
-LABEL_TAG = 'Tag'
-LABEL_SKILL = 'Skill'
-LABEL_PASSION = 'Passion'
-KING_SLAYER_TAG = dict(
-    zip(TAG_PROPERTIES, [str(uuid.uuid4()), NOW, 'King Slayer']))
-KING_SLAYER_LABELS = {LABEL_SKILL, LABEL_TAG}
-COLES_TAG = dict(
-    zip(TAG_PROPERTIES, [str(uuid.uuid4()), NOW, 'Space Explorer']))
-COLES_LABELS = {LABEL_TAG, LABEL_PASSION}
-"""
