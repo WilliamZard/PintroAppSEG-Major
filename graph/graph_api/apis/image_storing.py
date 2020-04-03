908853f18d514706b87aeb5e6aec937887545197
k1889501 <giulio.di_zio@kcl.ac.uk>
2020-04-03 18:56:13 +0100

    Delete bucket_name from functions parameter

--- graph/graph_api/apis/image_storing.py
+++ graph/graph_api/apis/image_storing.py
@@ -1,7 +1,7 @@
 import os
 from google.cloud import storage
 import uuid
-def upload_data_to_gcs(bucket_name, data):
+def upload_data_to_gcs(data):
     '''Function that store an image in a given bucket in GCP.
     Params:
         -bucket_name = the name of the GCP bucket where to store the image in.
@@ -11,6 +11,7 @@ def upload_data_to_gcs(bucket_name, data):
     '''
     try:
         client = storage.Client()#This will look for a GOOGLE_APPLICATION_CREDENTIALS env variable.
+        bucket_name = os.environ.get('IMAGES_BUCKET_NAME')
         bucket = client.bucket(bucket_name)
         target_key = str(uuid.uuid4())# unique file name in the bucket.
         bucket.blob(target_key).upload_from_string(data)
@@ -21,7 +22,7 @@ def upload_data_to_gcs(bucket_name, data):
 
     return ''
 
-def get_data_from_gcs(bucket_name, file_url):
+def get_data_from_gcs(file_url):
     '''Function that gets a file stored in GCP buckets and returns a bytes object.
     Params:
         -bucket_name = the name of the GCP bucket where to look for the given file.
@@ -35,6 +36,7 @@ def get_data_from_gcs(bucket_name, file_url):
     if(len(file_url) > 0):
         try:
             client = storage.Client()#This will look for a GOOGLE_APPLICATION_CREDENTIALS env variable.
+            bucket_name = os.environ.get('IMAGES_BUCKET_NAME')
             bucket = client.bucket(bucket_name)
             target_key = get_file_name_from_url(file_url)
             blob = bucket.get_blob(target_key).download_as_string()# get bucket data as blob
@@ -46,7 +48,7 @@ def get_data_from_gcs(bucket_name, file_url):
 
     return ''
 
-def delete_data_from_gcs(bucket_name, file_url):
+def delete_data_from_gcs(file_url):
     '''Function that deletes a file stored in GCP buckets.
     Params:
         -bucket_name = the name of the GCP bucket where to look for the given file.
@@ -57,13 +59,14 @@ def delete_data_from_gcs(bucket_name, file_url):
     if(len(file_url) > 0):
         try:
             client = storage.Client()#This will look for a GOOGLE_APPLICATION_CREDENTIALS env variable.
+            bucket_name = os.environ.get('IMAGES_BUCKET_NAME')
             bucket = client.bucket(bucket_name)
             target_key = get_file_name_from_url(file_url)
             bucket.delete_blob(target_key)
         except Exception as e:
             print(e)
 
-def update_data_from_gcs(bucket_name, old_file_url, new_data):
+def update_data_from_gcs(old_file_url, new_data):
     '''Function that update an image in a given bucket in GCP and returns its new url.
     Params:
         -bucket_name = the name of the GCP bucket where to store the image in.
@@ -76,6 +79,7 @@ def update_data_from_gcs(bucket_name, old_file_url, new_data):
     '''
     try:
         client = storage.Client()#This will look for a GOOGLE_APPLICATION_CREDENTIALS env variable.
+        bucket_name = os.environ.get('IMAGES_BUCKET_NAME')
         bucket = client.bucket(bucket_name)
         #delete the previously stored file.
         if(len(old_file_url) > 0):
