4b8c63337f35e09ab9a879429760b2d2b1fae43b
RaffWasTaken <47249441+RaffWasTaken@users.noreply.github.com>
2020-04-09 17:45:51 +0100

    Fix line indentation

--- Pintro/Components/GreyTag.js
+++ Pintro/Components/GreyTag.js
@@ -11,37 +11,35 @@ const GreyTag = props => {
             paddingHorizontal: 28,
             borderRadius: 24,
             marginHorizontal: 5,
-        };
+    };
 
-        const blackTag = {
-            borderColor: Colors.pintroBlack,
-            backgroundColor: Colors.pintroBlack,
-            borderWidth: 0.5,
-            paddingVertical: 10,
-            paddingHorizontal: 28,
-            borderRadius: 24,
-            marginHorizontal: 5,
-        };
+    const blackTag = {
+        borderColor: Colors.pintroBlack,
+        backgroundColor: Colors.pintroBlack,
+        borderWidth: 0.5,
+        paddingVertical: 10,
+        paddingHorizontal: 28,
+        borderRadius: 24,
+        marginHorizontal: 5,
+    };
 
-        const blackText = {
-            color: Colors.pintroBlack,
-            textAlign: 'center',
-            fontFamily: 'Poppins-Regular',
-            fontSize: 12
-        }
+    const blackText = {
+        color: Colors.pintroBlack,
+        textAlign: 'center',
+        fontFamily: 'Poppins-Regular',
+        fontSize: 12
+    }
 
-        const whiteText = {
-            color: Colors.pintroWhite,
-            textAlign: 'center',
-            fontFamily: 'Poppins-Regular',
-            fontSize: 12
-        }
+    const whiteText = {
+        color: Colors.pintroWhite,
+        textAlign: 'center',
+        fontFamily: 'Poppins-Regular',
+        fontSize: 12
+    }
+
     const[selected,select] = useState(props.initial);
     const[tagStyle,setTag] = useState((selected)? blackTag : greyTag);
     const[tagText,setText] = useState((selected)? whiteText : blackText);
-    
-
-    
 
     function onTagPress() {
         if(selected) {
