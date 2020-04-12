65b2c7f64798a7b5a87f98357115d07159d73567
sTranaeus <sebtranaeus@gmail.com>
2020-04-12 13:25:47 +0100

    Allow for inputting which database credentials to use.

--- neo4j_setup/neo4j_setup.py
+++ neo4j_setup/neo4j_setup.py
@@ -2,14 +2,12 @@
 Use this file to specify Cypher queries that should be ran manually
 after a database has been created. Similar to a startup script.
 """
-import os
+import sys
 from neo4j import GraphDatabase, Transaction, BoltStatementResult
 
 
-def connect() -> GraphDatabase.driver:
-    uri = os.getenv('NEO4J_URI')
+def connect(uri: str, password: str) -> GraphDatabase.driver:
     db_user = 'neo4j'
-    password = os.getenv("NEO4J_PASSWORD")
     driver = GraphDatabase.driver(uri, auth=(db_user, password))
     return driver
 
@@ -26,7 +24,8 @@ def create_full_text_indexes(tx: Transaction) -> None:
 
 
 if __name__ == '__main__':
-    driver = connect()
+    uri, password = sys.argv[1], sys.argv[2]
+    driver = connect(uri, password)
     with driver.session() as session:
         session.write_transaction(create_full_text_indexes)
     driver.close()
