f4723eeca55018696ee3556a72bd34606e1e2f19
JVK1890728 <jay.vachhani@outlook.com>
2020-04-13 07:18:43 +0100

    Base User page ready, awaiting test of Requests and other bug fixing

--- Pintro/Screens/UserAccountScreen.js
+++ Pintro/Screens/UserAccountScreen.js
@@ -1,15 +1,12 @@
 import React, { useState } from 'react';
 import { useSelector,useDispatch } from 'react-redux';
 import { Dimensions, StyleSheet, View, Text, Image, ScrollView, TouchableOpacity, FlatList } from 'react-native';
-import FollowMe from '../Components/FollowMe.js';
 import MsgMe from '../Components/MsgMe.js';
 import BlackTag from '../Components/BlackTag.js';
 import WhiteTag from '../Components/WhiteTag.js';
 import HelpMeWith from '../Components/HelpMeWith.js';
 import Edit from '../Components/Edit.js';
-import { fonts } from '../Constants/Fonts.js';
 import PencilBlack from '../Components/PencilBlack.js';
-import PencilWhite from '../Components/PencilWhite.js';
 import Colors from '../Constants/Colors.js';
 import JourneyPoint from '../Components/JourneyPoint.js';
 import TimelinePostComponent from '../Components/TimelinePostComponent.js';
@@ -70,7 +67,7 @@ const UserAccountScreen = props => {
     }
 
     function onConnectPress() {
-        //Do something
+        //Follow Request functionality
     }
 
     return(
@@ -128,6 +125,29 @@ const UserAccountScreen = props => {
                     <HelpMeWith props={props.HelpMeWith}>Crepe Investments</HelpMeWith>
                     <HelpMeWith props={props.HelpMeWith}>Home Workouts</HelpMeWith>
             </ScrollView>
+            <View>
+            <PencilBlack onPress={() => switchEditStory()}/>
+            <Text style={styles.myStoryHead}>My Story</Text>
+
+            <Text style={styles.more} onPress={() => onPressMore()}>{see}</Text>
+            </View>
+            <ScrollView style={styles.tagContainer} horizontal={true}>
+            <Text style={styles.myStoryHead}>I am passionate about</Text>
+            <PencilBlack onPress={() => switchEditPassions()}/>
+            </ScrollView>
+            <ScrollView style={styles.tagContainer} horizontal={true}>
+            <Text style={styles.myStoryHead}>I can help with</Text>
+            <PencilBlack onPress={() => switchEditHelpOthers()}/>
+            </ScrollView>
+            <View>
+                <View style={styles.rowContainer}>
+                <Text style={styles.journey}>Experience</Text>
+                <PencilBlack onPress={() => switchEditExperience()}/>
+                </View>
+
+
+            </View>
+
         </ScrollView>
     );
 };
@@ -137,13 +157,38 @@ const UserAccountScreen = props => {
 {userObj.job_title}
 {userObj.short_bio}
 {userObj.location}
+
+            <Text style={styles.storyContent} numberOfLines={lines}>
+                {userObj.story}
+            </Text>
+
+                    <BlackTag props={props.BlackTag}>{(userObj.passions[0]!==undefined)? userObj.passions[0].toUpperCase() : null}</BlackTag>
+                    <BlackTag props={props.BlackTag}>{(userObj.passions[1]!==undefined)? userObj.passions[1].toUpperCase() : null}</BlackTag>
+                    <BlackTag props={props.BlackTag}>{(userObj.passions[2]!==undefined)? userObj.passions[2].toUpperCase() : null}</BlackTag>
+                    <BlackTag props={props.BlackTag}>{(userObj.passions[3]!==undefined)? userObj.passions[0].toUpperCase() : null}</BlackTag>
+                    <BlackTag props={props.BlackTag}>{(userObj.passions[4]!==undefined)? userObj.passions[1].toUpperCase() : null}</BlackTag>
+                    <BlackTag props={props.BlackTag}>{(userObj.passions[5]!==undefined)? userObj.passions[2].toUpperCase() : null}</BlackTag>
+
+                    <WhiteTag props={props.WhiteTag}>{(userObj.help_Others[0]!==undefined)? userObj.help_Others[0].toUpperCase() : null}</WhiteTag>
+                    <WhiteTag props={props.WhiteTag}>{(userObj.help_Others[1]!==undefined)? userObj.help_Others[1].toUpperCase() : null}</WhiteTag>
+                    <WhiteTag props={props.WhiteTag}>{(userObj.help_Others[2]!==undefined)? userObj.help_Others[2].toUpperCase() : null}</WhiteTag>
+                    <WhiteTag props={props.WhiteTag}>{(userObj.help_Others[3]!==undefined)? userObj.help_Others[0].toUpperCase() : null}</WhiteTag>
+                    <WhiteTag props={props.WhiteTag}>{(userObj.help_Others[4]!==undefined)? userObj.help_Others[1].toUpperCase() : null}</WhiteTag>
+                    <WhiteTag props={props.WhiteTag}>{(userObj.help_Others[5]!==undefined)? userObj.help_Others[2].toUpperCase() : null}</WhiteTag>
+
+                    <JourneyPoint default={"Work Experience:"} userData={userObj.years_in_industry}/>
+                    <JourneyPoint default={"Industry:"} userData={userObj.Industry}/>
+                    <JourneyPoint default={"Current Company"} userData={userObj.current_Company}/>
+                    <JourneyPoint default={"Previous Company:"} userData={userObj.previous_Company}/>
+                    <JourneyPoint default={"Education:"} userData={userObj.university}/>
+                    <JourneyPoint default={"Academic Level:"} userData={userObj.academic_level}/>
 */
 
 const styles = StyleSheet.create({
     accountTop: {
         marginBottom: 0,
         marginTop: 15,
-        width: 320,
+        width: 322,
         height: 115,
         backgroundColor: Colors.pintroWhite,
     },
@@ -201,7 +246,7 @@ const styles = StyleSheet.create({
         marginLeft: 60,
     },
     currentJob: {
-        fontSize: 12,
+        fontSize: 14,
         color: Colors.pintroBlack,
         fontFamily: 'Poppins-Bold',
         marginLeft: 60,
@@ -218,102 +263,39 @@ const styles = StyleSheet.create({
         fontFamily: 'Poppins-Light',
         marginLeft: 60,
     },
+    helpContainer: {
+        flexDirection: 'row',
+        paddingLeft: 30,
+        paddingRight: 30,
+    },
     myStoryHead: {
-        fontSize: 12,
+        fontSize: 15,
+        paddingLeft: 20,
         color: Colors.pintroBlack,
         fontFamily: 'Poppins-Bold'        
+    },
+    storyContent: {
+        marginLeft: 30,
+        color: 'grey',
+        fontFamily: 'Poppins-Regular',
+        fontSize:12,
+        marginRight: 20,
+    },
+    more: {
+        marginLeft: 20,
+        color: Colors.pintroYellow,
+        fontFamily: 'Poppins-Bold',
+        fontSize:12
+    },
+    tagContainer: {
+        flexDirection: 'row',
+        marginTop: 10,
+        marginHorizontal: 0,
     },
     background: {
         backgroundColor: Colors.pintroWhite,
         flex: 1,
     }
 });
-
-/*
-            <View style={styles.accountTop}>
-                <Image source={require('../images/blank-profile-picture.png')} style={{ width: 60, height: 60}}/>
-                <View>
-                    <Text style={fonts.name_black}>John Doe</Text>
-                    <Text style={fonts.title_black}>Founder of John Doe industries</Text>
-                    <Text style={fonts.bio}>"Upon visualising tig bits I made my glorious snacc company"</Text>
-                    <Text style={fonts.location}>King's College London</Text>
-                    </View>
-            </View>
-            <View style={styles.rowContainer}>
-                <FollowMe props={props.FollowMe}>+ FOLLOW ME</FollowMe>
-                <MsgMe props={props.MsgMe}>MESSAGE ME</MsgMe>
-                <Edit props={props.Edit}>. . .</Edit>
-            </View>
-            <View>
-                <ScrollView> 
-                    <HelpMeWith props={props.HelpMeWith}>interdimensional travel</HelpMeWith>
-                    <HelpMeWith props={props.HelpMeWith}>find the szechuan sauce</HelpMeWith>
-                    <HelpMeWith props={props.HelpMeWith}>Heists</HelpMeWith>
-                </ScrollView>
-            </View>
-            <View>
-                <Text style={fonts.title_black}>My Story</Text>
-                <Text style={fonts.story}>
-                Some really really really long text in latin that 
-                sounds really fancy.
-                </Text>
-                <Text style={fonts.more_yellow}>More</Text>
-            </View>
-            <View>
-                <Text style={fonts.title_black}>Talk to me about</Text>
-                <View style={styles.rowContainer}>
-                    <BlackTag props={props.BlackTag}>Rick and Morty</BlackTag>
-                    <BlackTag props={props.BlackTag}>Comedy</BlackTag>
-                    <BlackTag props={props.BlackTag}>Memes</BlackTag>
-                </View>
-            </View>
-            <View>
-                <Text style={fonts.title_black}>I can help with</Text>
-                <View style={styles.rowContainer}>
-                    <WhiteTag props={props.WhiteTag}>Cooking</WhiteTag>
-                    <WhiteTag props={props.WhiteTag}>My golf game</WhiteTag>
-                    <WhiteTag props={props.WhiteTag}>Oooweee</WhiteTag>
-                </View>
-            </View> 
-            <View>
-                <Text style={fonts.title_black}>Experience</Text>
-                <View>
-                    <Text style={fonts.title_black}>Work Experience:</Text><Text style={fonts.story}>DC</Text>
-                    <Text style={fonts.title_black}>Industry:</Text><Text style={fonts.story}>Sending VKs</Text>
-                </View>
-            </View>
-            <View>
-                <Text style={fonts.title_black}>Groups</Text><Text style={fonts.more_white}>See all</Text>
-                <View style={styles.name_title}>
-                    <Image source={require('../images/blank-profile-picture.png')} />
-                    <Text style={fonts.title_black}>Group 1</Text>
-                    <Text style={fonts.story}>69 members</Text>
-                </View>
-                <View style={styles.name_title}>
-                    <Image source={require('../images/blank-profile-picture.png')} />
-                    <Text style={fonts.title_black}>Group 2</Text>
-                    <Text style={fonts.story}>42 members</Text>
-                </View>
-            </View>
-            <View>
-                <Text style={fonts.title_black}>Community</Text><Text style={fonts.more_white}>See all</Text>
-                <View>
-                    <Button><Image source={require('../images/blank-profile-picture.png')} style={{ width: 30, width: 30}}/></Button>
-                    <Button><Image source={require('../images/blank-profile-picture.png')} style={{ width: 30, width: 30}}/></Button>
-                    <Button><Image source={require('../images/blank-profile-picture.png')} style={{ width: 30, width: 30}}/></Button>
-                    <Button><Image source={require('../images/blank-profile-picture.png')} style={{ width: 30, width: 30}}/></Button>
-                    <Button><Image source={require('../images/blank-profile-picture.png')} style={{ width: 30, width: 30}}/></Button>
-                    <Button><Image source={require('../images/blank-profile-picture.png')} style={{ width: 30, width: 30}}/></Button>
-                </View>
-            </View>
-            <View>
-                <Text style={fonts.name_title}>Recommendations</Text>
-                <View>
-                    <Button><Image source={require('../images/blank-profile-picture.png')} style={{ width: 70, width: 70}}/></Button>
-                    <Button><Image source={require('../images/blank-profile-picture.png')} style={{ width: 70, width: 70}}/></Button>
-                    <Button><Image source={require('../images/blank-profile-picture.png')} style={{ width: 70, width: 70}}/></Button>
-                </View>
-            </View>
-*/
 
 export default UserAccountScreen;
